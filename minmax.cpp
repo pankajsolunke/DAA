5b0b6706509f441233249fffbb00522d43bea4a3
Pankaj Solunke <94754779+pankajsolunke@users.noreply.github.com>
2024-12-10 23:40:05 +0530

    Update minmax.cpp

--- minmax.cpp
+++ minmax.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main ()
 {
 
-int arr[10], n, i, max, min;
+int n, i, max, min;
 
 cout <<"Enter the size of the array : ";
 cin >> n;
+int arr[n];
 cout <<"Enter the elements of the array :";
 
 for (i = 0; i < n; i++)
