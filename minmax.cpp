7e9c086ee9487fb176216770188d07f63bbcffc9
Pankaj Solunke <94754779+pankajsolunke@users.noreply.github.com>
2024-12-11 00:06:11 +0530

    Update minmax.cpp

--- minmax.cpp
+++ minmax.cpp
@@ -10,23 +10,20 @@ cin >> n;
 int arr[n];
 cout <<"Enter the elements of the array :";
 
-for (i = 0; i < n; i++)
+for(i = 0; i < n; i++)
 cin >> arr[i];
+
 max = arr[0];
+min = arr[0];
 
 for (i = 0; i < n; i++)
 {
  if (max < arr[i])
  max = arr[i];
-}
-
-min = arr[0];
-
-for (i = 0; i < n; i++)
-{
  if (min > arr[i])
  min = arr[i];
 }
+
 cout <<"Largest element : "<< max<<endl;
 cout <<"Smallest element : "<< min;
 return 0;
